c5d8e1de6182047b6a1dc27ab7b6d5310cddd0ae
Molnár István <67184747+iiistvan@users.noreply.github.com>
2021-08-02 10:48:52 +0200

    2 nem teljes

--- testproject/film_register.py
+++ testproject/film_register.py
@@ -14,25 +14,23 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get("https://witty-hill-0acfceb03.azurestaticapps.net/film_register.html ")
 
 
+# központi időzítés
 def ts():
     time.sleep(3)
 
+
+# tesztadatok
 testdata = ['Black widow', '2021', '2020', 'https://www.youtube.com/watch?v=Fp9pNPdNwjI',
             'https://m.media-amazon.com/images/I/914MHuDfMSL._AC_UY327_FMwebp_QL65_.jpg',
             'https://www.imdb.com/title/tt3480822/']
 
-# movies = driver.find_elements_by_xpath('//h2[@class="center"]')
-# movies = driver.find_elements_by_class_name('container-movies')
-# movies = driver.find_elements_by_xpath('//*[@class="data-linktrailer"]')
-ts()
-# print(movies)
-# print(len(movies))
-
-# TC1: 24 db film
+# TC1: 24 db film ellenőrzése
+movies = driver.find_elements_by_xpath('//img[@src]')
+time.sleep(8)
+print(len(movies))
 # assert len(movies) == 24
 
-# TC2:
-
+# TC2: film regisztráció és ellenőrzés
 register = driver.find_element_by_xpath('//button[@class="mostra-container-cadastro"]')
 register.click()
 ts()
@@ -41,9 +39,10 @@ save_button = driver.find_element_by_xpath("//*[contains(text(), 'Save')]")
 inputs = driver.find_elements_by_xpath('//div[@class="container-cadastro-campo"]')
 ts()
 print(len(inputs))
-for e,i in enumerate(inputs):
+for e, i in enumerate(inputs):
     i.find_element_by_tag_name('input').send_keys(testdata[e])
     ts()
 save_button.click()
 ts()
-assert driver.find_element_by_xpath('//img[@src="https://m.media-amazon.com/images/I/914MHuDfMSL._AC_UY327_FMwebp_QL65_.jpg"]').is_displayed()
+assert driver.find_element_by_xpath(
+    '//img[@src="https://m.media-amazon.com/images/I/914MHuDfMSL._AC_UY327_FMwebp_QL65_.jpg"]').is_displayed()
