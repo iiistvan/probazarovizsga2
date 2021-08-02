fb22a774a19878e9e8d7da145bb9e32ec895ed9f
Molnár István <67184747+iiistvan@users.noreply.github.com>
2021-08-02 11:25:25 +0200

    3 még nem megy

--- testproject/guess_the_number.py
+++ testproject/guess_the_number.py
@@ -16,3 +16,21 @@ driver.get("https://witty-hill-0acfceb03.azurestaticapps.net/guess_the_number.ht
 
 def ts():
     time.sleep(1)
+
+input = driver.find_element_by_tag_name('input')
+guess = driver.find_element_by_xpath("//*[contains(text(), 'Guess')]")
+guess = driver.find_element_by_class_name('input-group-btn')
+
+for i in range(1,10):
+    input.clear()
+    ts()
+    input.send_keys(i)
+    ts()
+    guess.click()
+    ts()
+    # print(driver.find_element_by_xpath('//p[@class="alert alert-warning"]').get_attribute('value'))
+    if driver.find_element_by_xpath('//p[@class="alert alert-success"]').is_displayed():
+        szamlalo = i
+        break
+print(szamlalo)
+
